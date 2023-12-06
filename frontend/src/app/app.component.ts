750b13653deed06b3b7f0810476cdfc364f969d9
James Armer <jamesarmer2@gmail.com>
2023-12-06 16:27:38 +0530

    refactor: folder naming

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TextInputComponent } from './text-input/text-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [CommonModule, RouterOutlet, TextInputComponent],
 })
 export class AppComponent {
   title = 'secure-privacy-frontend';
